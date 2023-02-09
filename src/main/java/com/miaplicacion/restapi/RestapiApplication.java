1b9c0f8c4c84adf4d8d90053036eea306ed2e9f8
Lui Agüero <agueroluisalberto@gmail.com>
2023-02-09 18:34:44 -0300

    cors fix

--- src/main/java/com/miaplicacion/restapi/RestapiApplication.java
+++ src/main/java/com/miaplicacion/restapi/RestapiApplication.java
@@ -2,8 +2,21 @@ package com.miaplicacion.restapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+
 @SpringBootApplication
-public class RestapiApplication {
+public class RestapiApplication implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(RestapiApplication.class, args);
 	}
