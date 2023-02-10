6d0f323091cff83b836bd04696d7ecb0f929b3b7
Lui Agüero <agueroluisalberto@gmail.com>
2023-02-10 02:35:09 -0300

    cors delete

--- src/main/java/com/miaplicacion/restapi/RestapiApplication.java
+++ src/main/java/com/miaplicacion/restapi/RestapiApplication.java
@@ -1,19 +1,10 @@
 package com.miaplicacion.restapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableWebMvc
-@Configuration
+
 @SpringBootApplication
-public class RestapiApplication implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
-    }
+public class RestapiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestapiApplication.class, args);
 	}
