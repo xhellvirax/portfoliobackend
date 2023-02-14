c8aea83f1d9d140396bc7cbeb4f12d79c2c8fe28
Lui Agüero <agueroluisalberto@gmail.com>
2023-02-14 04:46:35 -0300

    cors

--- src/main/java/com/miaplicacion/restapi/WebConfig.java
+++ src/main/java/com/miaplicacion/restapi/WebConfig.java
@@ -1,11 +1,9 @@
 package com.miaplicacion.restapi;
+
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@CrossOrigin(origins = "https://portfolioback-yo68.onrender.com/")
 
 @EnableWebMvc
 @Configuration
