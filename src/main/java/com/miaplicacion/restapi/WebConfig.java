51bbbc7b4fd35631f41e112b71779664314d8a11
Lui Agüero <agueroluisalberto@gmail.com>
2023-02-13 23:32:34 -0300

    origin

--- src/main/java/com/miaplicacion/restapi/WebConfig.java
+++ src/main/java/com/miaplicacion/restapi/WebConfig.java
@@ -1,14 +1,20 @@
 package com.miaplicacion.restapi;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@CrossOrigin(origins = "https://portfolioangular-8b34a.firebaseapp.com/")
 
 @EnableWebMvc
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST")
+                .allowCredentials(false).maxAge(3600);
     }
 }
